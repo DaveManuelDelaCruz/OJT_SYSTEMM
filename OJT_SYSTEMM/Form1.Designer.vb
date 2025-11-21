4348532ef63465fb16da3da2a7817e72ca5f2e92
Arcie Cabiles <arciecabiles5@gmail.com>
2025-11-21 13:05:36 +0800

    nn

--- OJT_SYSTEMM/Form1.Designer.vb
+++ OJT_SYSTEMM/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class frmOpening
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,29 +22,18 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Button1 = New Button()
         SuspendLayout()
         ' 
-        ' Button1
-        ' 
-        Button1.Location = New Point(176, 176)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(415, 117)
-        Button1.TabIndex = 0
-        Button1.Text = "ENTER"
-        Button1.UseVisualStyleBackColor = True
-        ' 
-        ' Form1
+        ' frmOpening
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
-        Controls.Add(Button1)
-        Name = "Form1"
-        Text = "Form1"
+        BackColor = Color.Green
+        ClientSize = New Size(1262, 673)
+        Name = "frmOpening"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Welcome"
         ResumeLayout(False)
     End Sub
-
-    Friend WithEvents Button1 As Button
 
 End Class
--- OJT_SYSTEMM/Form1.vb
+++ OJT_SYSTEMM/Form1.vb
@@ -1,12 +1,3 @@
-﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'add comment
-        'test
-        '123
-        '456
-        'merge
+﻿Public Class frmOpening
 
-        'arcieczcASDSadfawdfsdfwfwdfwfw
-        'test
-    End Sub
 End Class
